0e3dc293225054261fdf0727b94b26096fb997ff
Andi <awliu2@uchicago.edu>
2022-06-22 22:14:56 -0700

    initial commit

---
+++ longestConsecutiveSequence.py
@@ -0,0 +1,10 @@
+"""
+Given an unsorted array of integers nums,
+return the length of the longest consecutive elements sequence.
+Must run in O(n) time
+"""
+from typing import List
+class Solution: 
+    def longestConsecutive(self, nums: List[int]) -> int:
+        return 1
+        
