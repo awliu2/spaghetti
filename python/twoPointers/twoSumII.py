fff356752dd00efdcf90ee6659e87bed9e14bd21
Andi Willow Liu <awliu2@uchicago.edu>
2022-07-28 16:09:00 -0500

    two pointers going inward from outside

--- python/twoPointers/twoSumII.py
+++ python/twoPointers/twoSumII.py
@@ -3,21 +3,17 @@ from typing import List
 class Solution:
     def twoSumII(self, numbers: List[int], target: int) -> List[int]:
         p1 = 0
-        p2 = 1
+        p2 = len(numbers) - 1
         rv = []
-        l = len(numbers)
-        while(p1 < (l - 1)):
-            while p2 < l:
-                if numbers[p1] + numbers[p2] == target:
-                    rv.extend([p1 + 1,p2 + 1])
-                    return rv
-                if numbers[p1] + numbers[p2] > target:
-                    p1 = p2 - 2
-                    p2 -= 1
-                    continue
-                p2 += 1
-            p1 += 1
-            p2 = p1 + 1
+        while(p1 < p2):
+            s = numbers[p1] + numbers[p2]
+            if s == target:
+                rv.extend([p1 + 1, p2 + 1])
+                break
+            elif s < target:
+                p1 += 1
+            else:
+                p2 -= 1
         return rv
 
 
@@ -36,6 +32,9 @@ nums3.extend([2,3])
 for i in range (100000):
     nums3.append(9)
 
+
+nums4 = [3,24,50,79,88,150,345]
+
 print("expecting [1, 2]")
 print(Solution().twoSumII(numbers, 9))
 print("expecting [3, 4]")
@@ -46,3 +45,7 @@ print("expecting [2, 4]")
 print(Solution().twoSumII(nums2, 13))
 print("expecting [100001,100002]")
 print(Solution().twoSumII(nums3,5))
+
+
+print("expecting [3,6]")
+print(Solution().twoSumII(nums4,200))
