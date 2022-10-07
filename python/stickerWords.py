fa65ac3c738a5b45693f45adce0a17c07aca890d
Andi Willow Liu <awliu2@uchicago.edu>
2022-10-06 23:42:20 -0500

    stuff

--- python/stickerWords.py
+++ python/stickerWords.py
@@ -2,4 +2,23 @@ from typing import List
 
 class Solution:
     def minStickers(self, stickers: List[str], target: str) -> int:
-        ...
+        need = {}
+        for ch in target:
+            need[ch] = 1 + need.get(ch, 0)
+        print(need)
+        
+        sticker_dict = {}
+        for sticker in stickers:
+            sticker_dict[sticker] = {}
+            for ch in sticker:
+                sticker_dict[sticker][ch] = 1 + sticker_dict[sticker].get(ch, 0)
+        
+        print(sticker_dict)
+
+
+stickers= ["with","example","science"]
+target = "thehat"
+
+
+print(Solution().minStickers(stickers, target))
+
