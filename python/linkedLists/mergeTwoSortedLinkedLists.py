8068960da67f437f809a8f7c91123ea51e00a92d
Andi Liu <awliu2@uchicago.edu>
2022-10-11 15:46:46 -0500

    more stuff

--- python/linkedLists/mergeTwoSortedLinkedLists.py
+++ python/linkedLists/mergeTwoSortedLinkedLists.py
@@ -20,6 +20,8 @@ class Solution:
             else:
                 tail.next = list2
                 list2 = list2.next
-            tail = tail.next    
+            tail = tail.next
         
         tail.next = list1 or list2
+        return head.next
+    
