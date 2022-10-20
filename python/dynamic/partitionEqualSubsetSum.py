1e4d0f4fbdb01b3e4ba0025ef34578e244734905
Andi Willow Liu <awliu2@uchicago.edu>
2022-10-20 16:42:06 -0500

    stuff

--- python/dynamic/partitionEqualSubsetSum.py
+++ python/dynamic/partitionEqualSubsetSum.py
@@ -3,4 +3,18 @@ from typing import List
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
         total_sum = sum(nums)
+        if total_sum % 2 == 1:
+            return False
         
+        sub_sum = total_sum // 2
+        dp = [sub_sum - nums[i] for i in len(nums)]
+
+        for i in len(nums):
+            ...
+
+
+
+
+nums =[1,5,11,5]
+
+print(Solution().canPartition(nums))
