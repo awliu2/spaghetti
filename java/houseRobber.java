ebbcd91e383741964ba361bf04cdc26bd020b5e0
Andi Liu <awliu2@uchicago.edu>
2022-10-30 17:53:17 -0500

    houserobber

---
+++ java/houseRobber.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if (n == 1) return nums[0];
+        int prevPrev = 0;
+        int prev = 0;
+        for (int i = 0; i < n; i++) {
+            int temp = prev;
+            prev = Math.max(prevPrev + nums[i], prev);
+            prevPrev = temp;
+        }
+        return prev;       
+    }
+}
