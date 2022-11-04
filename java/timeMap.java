363b92fcb75708adc0bdf240f44158a74e6638ce
Andi Willow Liu <awliu2@uchicago.edu>
2022-11-03 23:55:38 -0500

    timeMap

---
+++ java/timeMap.java
@@ -0,0 +1,35 @@
+import java.util.*;
+class TimeMap {
+    HashMap <String, TreeMap <Integer, String>> map;
+
+    public TimeMap() {
+        map = new HashMap <String, TreeMap <Integer, String>>();
+    }
+    
+    public void set(String key, String value, int timestamp) {
+        if (!map.containsKey(key)) map.put(key, new TreeMap <Integer, String>());
+
+        map.get(key).put(timestamp, value);
+
+    }
+    
+    public String get(String key, int timestamp) {
+        if (!map.containsKey(key))
+        {
+            return "";
+        }
+        Integer floorKey = map.get(key).floorKey(timestamp);
+        if (floorKey == null)
+        {
+            return "";
+        }
+        return map.get(key).get(floorKey);
+    }
+}
+
+/**
+ * Your TimeMap object will be instantiated and called as such:
+ * TimeMap obj = new TimeMap();
+ * obj.set(key,value,timestamp);
+ * String param_2 = obj.get(key,timestamp);
+ */
